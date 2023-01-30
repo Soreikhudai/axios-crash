40d210b0603496834a80a6dab7f88e8088c278b7
Soreikhudai <khudaisoreipam@gmail.com>
2023-01-30 21:43:31 +0530

    done

---
+++ main.js
@@ -0,0 +1,70 @@
+function save(event) {
+    event.preventDefault()
+    const name = event.target.username.value;
+    const email = event.target.emailId.value;
+    const phoneNo = event.target.phonenumber.value;
+    const obj = {
+        name,
+        email,
+        phoneNo
+    }
+    // localStorage.setItem(obj.email, JSON.stringify(obj))
+    axios.post("https://crudcrud.com/api/2ef924f8538544dfb8cc331e5d85e62d/data", obj)
+        .then((response) => {
+
+            showUserInScreen(response.data)
+        })
+        .catch((err) => {
+            document.body.innerHTML += "<h3>something went wrong</h3>"
+            console.log(err)
+        })
+}
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/2ef924f8538544dfb8cc331e5d85e62d/data")
+        .then((response) => {
+            for (var i = 0; i < response.data.length; i++) {
+                showUserInScreen(response.data[i])
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+
+
+
+
+
+function showUserInScreen(obj) {
+    const parentEle = document.getElementById("listOfItems")
+    const childEle = document.createElement("li")
+    childEle.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phoneNo;
+    parentEle.appendChild(childEle)
+
+const deleteBtn=document.createElement("input")
+deleteBtn.type="button"
+deleteBtn.value="delete"
+childEle.appendChild(deleteBtn)
+deleteBtn.onclick=()=>{
+    parentEle.removeChild(childEle)
+    axios.delete(`https://crudcrud.com/api/2ef924f8538544dfb8cc331e5d85e62d/data/${obj._id}`)
+    .then((response) => {
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+}
+
+
+
+
+
+
+
+
